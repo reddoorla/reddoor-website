ceb5651e253e8c0daec418a5d39890ca4f858436
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-08-29 15:49:03 -0700

    fixed unnecessary padding

--- src/routes/[[preview=preview]]/+page.svelte
+++ src/routes/[[preview=preview]]/+page.svelte
@@ -162,7 +162,7 @@
 <div class="w-screen bg-paper">
   <ContentWidth class="flex flex-col md:flex-row md:py-40 justify-between">
     <img src={printedReddoorLogo} alt="reddoor logo" class="w-full h-fit md:w-1/5  max-w-24 my-20"/>
-    <div class="w-full md:w-4/5 flex flex-col md:p-16 items-start">
+    <div class="w-full md:w-4/5 flex flex-col md:py-16 items-start">
       <h3 class="text-primary w-full ">Arm your brand with a clear story and compelling design.</h3>
       <div class="flex flex-row gap-6 mt-12">
         <a href="/contact">
