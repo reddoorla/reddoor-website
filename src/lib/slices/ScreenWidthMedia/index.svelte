65be10f1e809cce8591a3dd57e2b2238a62abbd7
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-12-12 09:49:21 -0800

    remove extra padding

--- src/lib/slices/ScreenWidthMedia/index.svelte
+++ src/lib/slices/ScreenWidthMedia/index.svelte
@@ -16,7 +16,7 @@
             <iframe 
 	  					title="background video" 
 	 					src={`https://player.vimeo.com/video/${slice.primary.vimeoid}?title=0${ slice.primary.loopvideo ? "&background=1&loop=1&autoplay=1&muted=1":""}`}
-	  					class="object-cover aspect-video w-screen mx-auto z-10 {slice.primary.hasPadding? "py-12":""}"
+	  					class="object-cover aspect-video w-screen mx-auto z-10"
 	  					frameborder="0"
               allow="autoplay; fullscreen;"
 					></iframe>
